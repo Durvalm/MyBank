81428271ce819d34d250a2339fd185e03a2b6583
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2024-03-13 18:13:25 -0400

    bank.py

---
+++ bank.py
@@ -0,0 +1,90 @@
+import datetime
+import os
+
+script_directory = os.path.dirname(os.path.realpath(__file__))
+
+class Money:
+
+    def __init__(self):
+        self.introduce()
+        self.amount = self.get_amount()
+        if self.amount is None:
+            return None
+        self.option = self.get_option()
+        if self.option is None:
+            return None
+        self.category = self.get_category()
+        if self.category is None:
+            return None
+        self.decription = self.get_description()
+        self.date = datetime.date.today()
+
+        self.add()
+
+
+    def introduce(self):
+        print("\nWelcome to the Bank program")
+        print("Enter the amount and select [income] or [spending]")
+    
+
+    def add(self):
+        if not self.option and self.amount:
+            return None
+        with open(f"{script_directory}/data.txt", "a") as file:
+            file.write(self.to_dict() + "\n")
+        print(f"{self.amount} added as {self.category} {self.option} in {self.date}")
+    
+    def to_dict(self):
+        return str({"amount": self.amount, "type": self.option,
+                    "date": self.date, "category": self.category, "description": self.decription})
+
+    def get_amount(self):
+        try:
+            amount = float(input("Enter Amount: "))
+        except ValueError:
+            print("Has to be number")
+            return None
+        return amount
+    
+    def get_description(self):
+        return input(f"More details about this {self.option}: ")
+
+    def get_option(self):
+        option = input("Enter if is income or spending: ").lower()
+        return self.check_option(option)
+
+
+    def check_option(self, option):
+        if option == "income" or option == "spending":
+            return option
+        else:
+            print("invalid type")
+            return None
+
+    def get_category(self):
+        print(f"\nNow select what category is this {self.option}")
+        if self.option == "income":
+            categories = self.income_categories()
+        else:
+            categories = self.spending_categories()
+
+        print("Categories:", *categories)
+
+        category = input("Select category: ").lower()
+
+        if category not in categories:
+            return None
+        return category
+
+
+    def income_categories(self):
+        categories = ["work", "financial_aid", "family", "sell", "other"]
+        return categories
+    
+    def spending_categories(self):
+        categories = ["transportation", "groceries", "eating_out", "shopping", "app_subscriptions",
+                      "education", "utilities", "rent", "cellphone", "hobbies", "fitness", "other"]
+        return categories
+
+
+money = Money()
